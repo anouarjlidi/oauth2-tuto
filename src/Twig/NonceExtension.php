03d65c615227b7ab1dcc310eaf30eb6cd2948626
ANOUAR JLIDI <anouarjlidi@gmail.com>
2018-10-11 15:43:19 +0200

    Mettre en place yarn, webpack

---
+++ src/Twig/NonceExtension.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HSE-CORP
+ * Date: 22/09/2018
+ * Time: 15:30
+ */
+
+namespace App\Twig;
+
+use App\Security\NonceGenerator;
+
+/**
+ * Class NonceExtension
+ *
+ * @package App\Twig
+ */
+class NonceExtension extends \Twig_Extension
+{
+    /**
+     * @var NonceGenerator
+     */
+    private $nonceGenerator;
+
+    /**
+     * NonceExtension constructor.
+     * @param NonceGenerator $nonceGenerator
+     */
+    public function __construct(NonceGenerator $nonceGenerator)
+    {
+        $this->nonceGenerator = $nonceGenerator;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFunctions() : array
+    {
+        return [
+            new \Twig_SimpleFunction('csp_nonce', [$this, 'getNonce']),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getNonce() : string
+    {
+        return $this->nonceGenerator->getNonce();
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return 'nonce_extension';
+    }
+}
